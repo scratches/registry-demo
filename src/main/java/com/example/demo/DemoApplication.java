21c3003d6db334be218785d0308f77eff8f79ea1
Dave Syer <dsyer@vmware.com>
2023-03-10 10:28:23 +0000

    Native image

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,23 @@
 package com.example.demo;
 
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.guice.annotation.EnableGuiceModules;
 
 import com.google.inject.AbstractModule;
 
 @SpringBootApplication
 @EnableGuiceModules
+@ImportRuntimeHints(DemoRuntimeHints.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		// System.setProperty("spring.aot.enabled", "true");
 		System.err.println(SpringApplication.run(DemoApplication.class, args).getBean(Spam.class));
 	}
 
@@ -25,6 +31,21 @@ public class DemoApplication {
 		return new Spam(service);
 	}
 
+}
+
+class DemoRuntimeHints implements RuntimeHintsRegistrar {
+
+	@Override
+	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+		hints.reflection().registerType(Integer.class, MemberCategory.INVOKE_DECLARED_METHODS);
+		hints.reflection().registerType(Long.class, MemberCategory.INVOKE_DECLARED_METHODS);
+		hints.reflection().registerType(Double.class, MemberCategory.INVOKE_DECLARED_METHODS);
+		hints.reflection().registerType(Float.class, MemberCategory.INVOKE_DECLARED_METHODS);
+		hints.reflection().registerType(Boolean.class, MemberCategory.INVOKE_DECLARED_METHODS);
+		hints.reflection().registerType(Byte.class, MemberCategory.INVOKE_DECLARED_METHODS);
+		hints.reflection().registerType(Short.class, MemberCategory.INVOKE_DECLARED_METHODS);
+	}
+	
 }
 
 class MyModule extends AbstractModule {
---
+++ src/test/java/com/example/demo/EnableGuiceModulesTests.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.guice.annotation.EnableGuiceModules;
+import org.springframework.guice.annotation.GuiceModule;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+/**
+ * @author Dave Syer
+ *
+ */
+public class EnableGuiceModulesTests {
+
+	@AfterEach
+	public void cleanUp() {
+		System.clearProperty("spring.guice.dedup");
+	}
+
+	@Test
+	public void test() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
+		assertThat(context.getBean(Foo.class)).isNotNull();
+		context.close();
+	}
+
+	@Test
+	public void testWithDedupFeatureEnabled() {
+		System.setProperty("spring.guice.dedup", "true");
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
+		assertThat(context.getBean(Foo.class)).isNotNull();
+		context.close();
+	}
+
+	@Test
+	public void module() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ModuleConfig.class);
+		assertThat(context.getBean(Foo.class)).isNotNull();
+		context.close();
+	}
+
+	@Test
+	public void moduleBean() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ModuleBeanConfig.class);
+		assertThat(context.getBean(Foo.class)).isNotNull();
+		context.close();
+	}
+
+	@Test
+	public void testInjectorCreationDoesNotCauseCircularDependencyError() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MySpringConfig.class);
+		assertThat(context.getBean(SpringProvidedBean.class)).isNotNull();
+		context.close();
+	}
+
+	interface Service {
+
+	}
+
+	protected static class MyService implements Service {
+
+	}
+
+	public static class Foo {
+
+		@Inject
+		public Foo(@Named("service") Service service) {
+			service.toString();
+		}
+
+	}
+
+	@Configuration
+	@EnableGuiceModules
+	@GuiceModule(excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, value = Foo.class))
+	protected static class TestConfig {
+
+		@Autowired
+		private Injector injector;
+
+		@Bean
+		public Foo foo() {
+			return this.injector.getInstance(Foo.class);
+		}
+
+		@Bean
+		public Service service() {
+			return new MyService();
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@EnableGuiceModules
+	protected static class ModuleConfig extends AbstractModule {
+
+		@Override
+		protected void configure() {
+			bind(Service.class).to(MyService.class);
+		}
+
+		@Bean
+		public Foo service(Service service) {
+			return new Foo(service);
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@EnableGuiceModules
+	protected static class ModuleBeanConfig {
+
+		@Bean
+		public static MyModule module() {
+			return new MyModule();
+		}
+
+		@Bean
+		public Foo service(Service service) {
+			return new Foo(service);
+		}
+
+	}
+
+	protected static class MyModule extends AbstractModule {
+
+		@Override
+		protected void configure() {
+			bind(Service.class).to(MyService.class);
+		}
+
+	}
+
+	public static class SpringProvidedBean {
+
+		public SpringProvidedBean(GuiceProvidedBean guiceProvidedBean) {
+		}
+
+	}
+
+	public static class GuiceProvidedBean {
+
+	}
+
+	public static class GuiceService {
+
+		@Inject
+		public GuiceService(SpringProvidedBean springProvidedBean) {
+		}
+
+	}
+
+	public static class MyGuiceModule extends AbstractModule {
+
+		@Override
+		protected void configure() {
+			bind(GuiceService.class).asEagerSingleton();
+		}
+
+		@Provides
+		@Singleton
+		public GuiceProvidedBean guiceProvidedBean() {
+			return new GuiceProvidedBean();
+		}
+
+	}
+
+	@Configuration
+	@EnableGuiceModules
+	public static class MySpringConfig {
+
+		@Bean
+		public SpringProvidedBean baz(GuiceProvidedBean guiceProvidedBean) {
+			return new SpringProvidedBean(guiceProvidedBean);
+		}
+
+		@Bean
+		public static MyGuiceModule bazModule() {
+			return new MyGuiceModule();
+		}
+
+	}
+
+}
